f759c37c0224a52c70f544e7fff0d01d42bc1e31
Graham Farrell <grahamfarrell1984@gmail.com>
2020-04-25 21:39:50 +0100

    Add code for when P or D keys are pressed.

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -7,8 +7,12 @@ GameScene::GameScene(SceneManager& sceneManager, const Scene::Name name)
 
 void GameScene::processInput(const Keyboard& keyboard)
 {
-    if (keyboard.checkKeyAndState(sf::Keyboard::Enter, State::PRESS)) {
+    if (keyboard.checkKeyAndState(sf::Keyboard::P, State::PRESS)) {
         requestSceneChange(Scene::Action::PUSH, Scene::Name::PAUSE);
+    }
+
+    if (keyboard.checkKeyAndState(sf::Keyboard::D, State::PRESS)) {
+        requestSceneChange(Scene::Action::PUSH, Scene::Name::GAMEOVER);
     }
 }
 
