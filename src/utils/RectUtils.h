a8c33814ccdd13d63c97ff20cbb407cceecbf253
Graham Farrell <grahamfarrell1984@gmail.com>
2020-04-30 04:00:49 +0100

    Add comment explaining collision method logic.

--- src/utils/RectUtils.h
+++ src/utils/RectUtils.h
@@ -6,5 +6,10 @@ namespace Utils {
     static bool isIntersecting(const Rect& a, const Rect& b) noexcept
     {
         return (a.x + a.w) >= b.x && a.x <= (b.x + b.w) && (a.y + a.h) >= b.y && a.y <= (b.y + b.h);
+        /* If the top right corner of rectangle a is greater than or equal to the top left corner of rectangle b
+         * and the top left corner of rectangle a is less than or equal to the top right corner of rectangle b
+         * and the bottom left corner of rectangle a is greater than or equal to the top left corner of rectangle b
+         * and the top left corner of rectangle a is less than or equal to the bottom left corner of rectangle b
+         * them a collision has occurred. */
     }
 } // NS Utils::Rect
