8bf02fa6b1ef64a8dc5c51647846d83808503be8
Graham Farrell <grahamfarrell1984@gmail.com>
2020-05-04 16:41:04 +0100

    Add comments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "App.h"
 
+// Main method - entry point to the game
 int main()
 {
-    App app;
+    App app; // Create an instance of the App class
+
+    // Check if the game has been set up correctly
     if (app.setup()) {
+        // If it has been set up correctly then call the run() method
         app.run();
     }
     return 0;
