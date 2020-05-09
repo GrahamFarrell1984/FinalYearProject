70ce974f0effd17b4ead9754a27b5a623d5af962
Graham Farrell <grahamfarrell1984@gmail.com>
2020-05-09 11:51:51 +0100

    Rename assets.

--- src/AssetData.h
+++ src/AssetData.h
@@ -9,18 +9,18 @@ struct Data
 // NOTE : Make sure indexes are unique.
 namespace Assets {
     namespace Texture {
-        static constexpr Data TEXA   { 0, "assets/texture/TEX_A.png" };
-        static constexpr Data PLAYER { 1, "assets/texture/player.png" };
+        static constexpr Data TEXA   { 0, "assets/texture/scenes.png" };
+        static constexpr Data PLAYER { 1, "assets/texture/entities.png" };
         static constexpr Data MUMMY  { 2, "assets/texture/mummy.png" };
         static constexpr Data LEVEL  { 3, "assets/texture/level.png" };
     }  // namespace Texture
 
     namespace Font {
-        static constexpr Data FONTA{ 0, "assets/font/Minecraft.ttf" };
+        static constexpr Data FONTA{ 0, "assets/font/game-font.ttf" };
     }  // namespace Font
 
     namespace Sfx {
-        static constexpr Data SFXA{ 0, "assets/sfx/SFX_A.wav" };
+        static constexpr Data SFXA{ 0, "assets/sfx/select.wav" };
         static constexpr Data SFXB{ 1, "assets/sfx/rescue.ogg" };
         static constexpr Data SFXC{ 2, "assets/sfx/explosion.ogg" };
         static constexpr Data SFXD{ 3, "assets/sfx/shoot.ogg" };
@@ -28,8 +28,8 @@ namespace Assets {
     }  // namespace Sfx
 
     namespace Music {
-        static constexpr Data MUSICA{ 0, "assets/music/MUSIC_A.ogg" };
-        static constexpr Data MUSICB{ 1, "assets/music/killing-zombies-loop.ogg" };
+        static constexpr Data MUSICA{ 0, "assets/music/main-title.ogg" };
+        static constexpr Data MUSICB{ 1, "assets/music/killing-zombies.ogg" };
     }  // namespace Sfx
 
 
